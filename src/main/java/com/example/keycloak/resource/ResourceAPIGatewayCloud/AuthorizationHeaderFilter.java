3cbe23a8b86c9620603562fcb4af66a22343a35e
Christopher-OOC <olojedechristopher24@gmail.com>
2024-10-15 20:29:59 +0100

    commit

--- src/main/java/com/example/keycloak/resource/ResourceAPIGatewayCloud/AuthorizationHeaderFilter.java
+++ src/main/java/com/example/keycloak/resource/ResourceAPIGatewayCloud/AuthorizationHeaderFilter.java
@@ -1,4 +1,16 @@
 package com.example.keycloak.resource.ResourceAPIGatewayCloud;
 
-public class AuthorizationHeaderFilter {
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+
+public class AuthorizationHeaderFilter extends AbstractGatewayFilterFactory<AuthorizationHeaderFilter.Config> {
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        return null;
+    }
+
+    public static class Config {
+
+    }
 }
