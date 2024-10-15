c32655bf9d5374980d81e9fe48e64a0e111f5843
Christopher-OOC <olojedechristopher24@gmail.com>
2024-10-15 23:04:26 +0100

    commit

--- src/main/java/com/example/keycloak/resource/ResourceAPIGatewayCloud/AuthorizationHeaderFilter.java
+++ src/main/java/com/example/keycloak/resource/ResourceAPIGatewayCloud/AuthorizationHeaderFilter.java
@@ -1,7 +1,13 @@
 package com.example.keycloak.resource.ResourceAPIGatewayCloud;
 
+import io.jsonwebtoken.Jwt;
+import io.jsonwebtoken.JwtParser;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
@@ -9,8 +15,20 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.util.Base64;
+import java.util.Map;
+
 @Component
 public class AuthorizationHeaderFilter extends AbstractGatewayFilterFactory<AuthorizationHeaderFilter.Config> {
+
+    @Autowired
+    private Environment environment;
+
+    public AuthorizationHeaderFilter() {
+        super(Config.class);
+    }
 
     @Override
     public GatewayFilter apply(Config config) {
@@ -23,6 +41,14 @@ public class AuthorizationHeaderFilter extends AbstractGatewayFilterFactory<Auth
                 return onError(exchange, "No Authorization header", HttpStatus.UNAUTHORIZED);
             }
 
+            String authorizationHeader = request.getHeaders().get("Authorization").get(0);
+            String jwt = authorizationHeader.replace("Bearer ", "");
+
+            if (!isJwtValid(jwt)) {
+                return onError(exchange, "JWT not valid", HttpStatus.UNAUTHORIZED);
+            }
+
+
             return chain.filter(exchange);
         });
     }
@@ -32,6 +58,38 @@ public class AuthorizationHeaderFilter extends AbstractGatewayFilterFactory<Auth
         response.setStatusCode(httpStatus);
 
         return response.setComplete();
+    }
+
+    private boolean isJwtValid(String jwt) {
+
+        boolean isValid = true;
+        String subject = null;
+
+        String tokenSecret = environment.getProperty("token.secret");
+        byte[] secretKeyBytes = Base64.getDecoder().decode(tokenSecret.getBytes());
+        SecretKey secretKey = new SecretKeySpec(secretKeyBytes, SignatureAlgorithm.HS512.getJcaName());
+
+        try {
+
+            JwtParser jwtParser = Jwts.parser()
+                    .setSigningKey(secretKey)
+                    .build();
+
+            Jwt<?, ?> parse = jwtParser.parse(jwt);
+            Map<String, Object> payload = (Map<String, Object>) parse.getPayload();
+            subject =  (String) payload.get("subject");
+
+
+        }
+        catch (Exception ex) {
+            isValid = false;
+        }
+
+        if (subject == null || subject.isEmpty()) {
+            isValid = false;
+        }
+
+        return isValid;
     }
 
     public static class Config {
