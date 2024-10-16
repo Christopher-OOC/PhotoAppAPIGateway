7cd73d242caca4fc8640bf31ba1116c6bdb7d3ae
Christopher-OOC <olojedechristopher24@gmail.com>
2024-10-16 21:42:02 +0100

    commit

--- src/main/java/com/example/keycloak/resource/ResourceAPIGatewayCloud/GlobalFiltersConfiguration.java
+++ src/main/java/com/example/keycloak/resource/ResourceAPIGatewayCloud/GlobalFiltersConfiguration.java
@@ -1,13 +1,19 @@
 package com.example.keycloak.resource.ResourceAPIGatewayCloud;
 
 import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class GlobalFiltersConfiguration {
 
+    @Bean
     public GlobalFilter secondPreFilter() {
 
+        return ((exchange, chain) -> {
+
+            return chain.filter(exchange);
+        });
 
     }
 
