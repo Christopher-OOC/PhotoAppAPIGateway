ec55e7c7d70e0c22234fdedf5d21359250f3f003
Christopher-OOC <olojedechristopher24@gmail.com>
2024-10-16 19:41:47 +0100

    commit

--- src/main/java/com/example/keycloak/resource/ResourceAPIGatewayCloud/MyPreFilter.java
+++ src/main/java/com/example/keycloak/resource/ResourceAPIGatewayCloud/MyPreFilter.java
@@ -5,7 +5,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
