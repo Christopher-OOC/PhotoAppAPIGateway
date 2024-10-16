95794039bf223ce979cd4fb96462e0e8e23d9f3b
Christopher-OOC <olojedechristopher24@gmail.com>
2024-10-16 21:37:29 +0100

    commit

---
+++ src/main/java/com/example/keycloak/resource/ResourceAPIGatewayCloud/GlobalFiltersConfiguration.java
@@ -0,0 +1,13 @@
+package com.example.keycloak.resource.ResourceAPIGatewayCloud;
+
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GlobalFiltersConfiguration {
+
+    public GlobalFilter secondPreFilter() {
+
+    }
+
+}
